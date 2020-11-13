2be41077f6959f43affb7edf5c8192bf497d0f3c
Nator Verinumbe <natverior1@gmail.com>
2020-11-13 06:40:17 +0100

    wrote some code, let's see how it works

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Author struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+type Book struct {
+	ID     string  `json: "id"`
+	Isbn   string  `json: "isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json: "author"`
+}
+
+func main() {
+
+	mybook1 := Book{
+		ID:    "1",
+		Isbn:  "23533342",
+		Title: "Success",
+		Author: &Author{
+			FirstName: "Nator",
+			LastName:  "Verinumbe",
+		},
+	}
+
+	myRouter := mux.NewRouter()
+	myRouter.HandleFunc("/api/books", mybook1.getBooks).Methods("GET")
+
+	http.ListenAndServe(":8080", myRouter)
+}
+
+func (book *Book) getBooks(res http.ResponseWriter, req *http.Request) {
+	_ = json.NewEncoder(res).Encode(book)
+}
